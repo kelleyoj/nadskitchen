f3ef9435f3fa085059b57806ae534f7249d36ca9
OJ <kelleyoj@gmail.com>
2018-11-24 22:05:49 -0500

    change style

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ var test_2 = $("#test-2");
 
 
 $(document).ready(function () {
-    $("#home").on("click", function () {
+    $("#menu").on("click", function () {
         $(".bg-2").css("background-color", "rgb(151, 75, 75)");
         test_1.css("display","grid");
         test_2.css("display","none");
     })
 
-    $("#menu").on("click", function () {
+    $("#contact").on("click", function () {
         $(".bg-2").css("background-color", "green");
         test_1.css("display","none");
         test_2.css("display","grid");
